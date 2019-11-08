88cee16df42c0a5b33ccd94e9b7722e57ccc72b9
nahuel-fed <fgonz1998@gmail.com>
2019-11-08 14:41:14 -0300

    Reordena el código
    Se hizo que el código tenga otro aspecto. Se borraron cosas inútiles y se escribió código que se necesitaba reescribir

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -1,7 +1,8 @@
 /*
 TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
 Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
-Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
+Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y 
+el promedio del grupo familiar.
 
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar
      cómo en MDN).
@@ -18,15 +19,18 @@ $botonIngresar.onclick = function () {
      let divFamilia = document.querySelector("div#divFamilia")
      console.log(divFamilia)
 
+     if (numeroDelGrupoFamiliar>0&&numeroDelGrupoFamiliar!=null){
+          document.querySelector("div#divFamilia").style= "display:block"
+     }
+
+
 
      //divFamilia.innerText = 'Ingresá las edades de tus familiares en el siguiente formulario:'
      //nodoPaginaTexto.appendChild(nuevo)
-     /*
+     
 for (i = 1; i <= numeroDelGrupoFamiliar; i++) {
-     let nodoPaginaInput = document.querySelector('body')
-
+     let formularioFamilia =document.querySelector('form#formulario-familiar')
      let nuevoFormulario = document.createElement("form")
-
      let nuevoInput = document.createElement("input")
      nuevoInput.id = "inputFamilia"
      nuevoInput.type = 'number'
@@ -34,25 +38,36 @@ for (i = 1; i <= numeroDelGrupoFamiliar; i++) {
      let nuevoLabel = document.createElement("label")
      nuevoLabel.innerText = ("Integrante " + i + ': ')
 
-     nodoPaginaInput.appendChild(nuevoFormulario)
+     formularioFamilia.appendChild(nuevoFormulario)
+     
      nuevoFormulario.appendChild(nuevoLabel)
      nuevoFormulario.appendChild(nuevoInput)
 
-               if (i == numeroDelGrupoFamiliar) { 
-                    let $botonConfirmar = document.createElement("button")
-                    $botonConfirmar.id = 'botonConfirmar'
-                    $botonConfirmar.innerText = 'Confirmar'
-                    nodoPaginaInput.appendChild($botonConfirmar)*/
 }
 
-//return false
+
+return false
+
+}    
+
+
+
+//
 
 
 /*
+
+               /*if (i == numeroDelGrupoFamiliar) { 
+                    let $botonConfirmar = document.createElement("button")
+                    $botonConfirmar.id = 'botonConfirmar'
+                    $botonConfirmar.innerText = 'Confirmar'
+                    nodoPaginaInput.appendChild($botonConfirmar)}
 TAREA:
 Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de
 cada integrante de la familia que trabaje. Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario
 anual, menor salario anual, salario anual promedio y salario mensual promedio.
 
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
+
+
 */
