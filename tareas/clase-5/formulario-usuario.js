7be7785e9c442e929e37f5c10675e30b065c3f18
nahuel-fed <fgonz1998@gmail.com>
2019-11-12 16:15:47 -0300

    pule la acción del botón
    necesito un mejor orden, y tambien cambiar nombres de variables para que el archivo tenga una mejor legibilidad.

--- tareas/clase-5/formulario-usuario.js
+++ tareas/clase-5/formulario-usuario.js
@@ -2,32 +2,29 @@
 let $boton = document.querySelector("#boton-ingresar")
 
 $boton.onclick = function () {
-    let nombre = document.querySelector("#primer-nombre").value
-    let segundoNombre = document.querySelector("#segundo-nombre").value
-    let apellido = document.querySelector("#apellido").value
-    let edad = document.querySelector("#edad").value
-    let nodoDelDocumento = document.querySelector("body")
-    let parrafoNuevo = document.createElement("p")
-    
-    parrafoNuevo.innerText = "Hola " + primerNombre + ", tus datos estarán ordenados abajo:"
+  let segundoNombre = document.querySelector("#segundo-nombre").value
+  let nombre = document.querySelector("#primer-nombre").value
+  let apellido = document.querySelector("#apellido").value
+  let edad = document.querySelector("#edad").value
+  let nodoDelDocumento = document.querySelector("body")
+  let parrafoNuevo = document.createElement("p")
+  //nodoDelDocumento.appendChild(parrafoNuevo)
+  parrafoNuevo.innerText = "Hola " + nombre + ", tus datos estarán ordenados abajo:"
 
-console.log(nombre+segundoNombre+apellido+edad)
+  //  console.log(nombre + segundoNombre + apellido + edad)
 
+  let parrafoPrimerNombre = "Primer nombre: " + nombre
+  let parrafoSegundoNombre = "Segundo nombre: " + segundoNombre
+  let parrafoApellido = "Apellido: " + apellido
+  let parrafoEdad = "Edad: " + edad
 
-
-/*
-  parrafoPrimerNombre = "Primer nombre: " + nombre
-    parrafoSegundoNombre = "Segundo nombre: " + segundoNombre
-    parrafoApellido = "Apellido: " + apellido
-    parrafoEdad = "Edad: " + edad
-
-    nodoDelDocumento.appendChild(nodoDelDocumento)
+  nodoDelDocumento.appendChild(parrafoNuevo)
   parrafoNuevo.appendChild(parrafoPrimerNombre)
-    parrafoNuevo.appendChild(parrafoSegundoNombre)
-    parrafoNuevo.appendChild(parrafoApellido)
-    parrafoNuevo.appendChild(parrafoEdad)
+  parrafoNuevo.appendChild(parrafoSegundoNombre)
+  parrafoNuevo.appendChild(parrafoApellido)
+  parrafoNuevo.appendChild(parrafoEdad)
 
   document.querySelector("bienvenido").innerText = "Bienvenido, " + primerNombre + "!"
-*/
-    return false
+
+  return false
 }
