db66a83a211e5aecbd6c2081a63009fc02f243f7
nahuel-fed <fgonz1998@gmail.com>
2019-11-13 18:34:47 -0300

    Reescribe el código
    No había leído del todo bien la consigna del ejercicio

--- tareas/clase-5/formulario-usuario.js
+++ tareas/clase-5/formulario-usuario.js
@@ -9,28 +9,14 @@ $boton.onclick = function () {
   let nodoDelDocumento = document.querySelector("body")
   let parrafoNuevo = document.createElement("p")
   parrafoNuevo.id = "parrafo-nuevo"
-  //nodoDelDocumento.appendChild(parrafoNuevo)
   parrafoNuevo.innerText = "Hola " + nombre + ", tus datos estarán ordenados abajo:"
-
-  //  console.log(nombre + segundoNombre + apellido + edad)
-
+  let datosResultados = (nombre + " " + segundoNombre + " " + apellido + " " + edad)
 
 
-  let oracionPrimerNombre = document.createElement("p")
-  oracionPrimerNombre.innerText = ("Primer nombre: " + nombre)
-  let oracionSegundoNombre = document.createElement("p")
-  oracionSegundoNombre.innerText = ("Segundo nombre: " + segundoNombre)
-  let oracionApellido = document.createElement("p")
-  oracionApellido.innerText = ("Apellido: " + apellido)
-  let oracionEdad = document.createElement("p")
-  oracionEdad.innerText = ("Edad: " + edad)
 
-  nodoDelDocumento.appendChild(parrafoNuevo)
-  parrafoNuevo.appendChild(oracionPrimerNombre)
-  parrafoNuevo.appendChild(oracionSegundoNombre)
-  parrafoNuevo.appendChild(oracionApellido)
-  parrafoNuevo.appendChild(oracionEdad)
-
+  let inputResultado = document.querySelector("#texto-resultado")
+  inputResultado.style = "display:block"
+  inputResultado.value = datosResultados
   document.querySelector("#bienvenido").innerText = "Bienvenido, " + nombre + "!"
 
   return false
