3ea693ad1c831539aa135943c96d35adbb118d6c
nahuel-fed <fgonz1998@gmail.com>
2019-11-15 18:41:43 -0300

    Agrega botones, arregla bugs, y styles
    necesito que el usuario tenga a mano las cosas que necesita, y las que no, que desaparezcan
    Los forms uno encima del otro para una mayor prolijidad de la página
    Algunas cosas no funcionaban por los nombres que tenían
    crea boton para empezar de nuevo

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -12,68 +12,81 @@ Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como
 
 
 
-let $botonEntrar = document.querySelector("#boton-entrar")
+let $botonAceptar = document.querySelector("#boton-entrar")
 
-$botonAceptar = function () {
-     let crearInput = document.createElement("input")
-     crearInput.id = "input-sueldo"
-     let crearLabel = document.createElement("label")
-     crearLabel.id = "label-sueldo"
-     let nodoPrincipal = document.querySelector("div")
+$botonAceptar.onclick = function () {
 
      let numeroDeLaFamilia = document.querySelector("#cantidad-de-personas").value
+     let nodoPrincipal = document.querySelector("div#bienvenida")
+     for (i = 1; i <= numeroDeLaFamilia; i++) {
+          let crearForm = document.createElement("form")
+          let crearInput = document.createElement("input")
+          crearInput.id = "input-sueldo"
+          let crearLabel = document.createElement("label")
+          crearLabel.id = "label-sueldo"
+          crearLabel.innerText = "Ingresá el sueldo del integrante " + i + " :       "
 
-     for (i = 0; i <= numeroDeLaFamilia; i++) {
+          nodoPrincipal.appendChild(crearForm)
           nodoPrincipal.appendChild(crearLabel)
-          crearLabel.innerText = "Ingresá el sueldo del integrante " + i
+
           nodoPrincipal.appendChild(crearInput)
      }
+
+     document.querySelector("#cantidad-de-personas").style = "display:none"
+     document.querySelector("#boton-entrar").style = "display:none"
 
 
      document.querySelector("#boton-calcular").style = "display=block"
 
-
-
-
-
-
-
+     let $calcularDeNuevo = document.querySelector("#empezar-de-nuevo").style= "display:block"
 
 }
 
 
 
 
-// let sueldoPromedio
-// let sueldoMayor
-// let sueldoMenor
-
 let $botonCalcular = document.querySelector("#boton-calcular")
 
-$botonCalcular = function () {
+$botonCalcular.onclick = function () {
      let todosLosSueldos = document.querySelectorAll("#input-sueldo")
      let mayor = 0
      let menor = 0
      let promedio = 0
-     let divisor
-     for (let i of todosLosSueldos.value) {
-          if (i < menor) {
-               menor = i
+     let divisor = 0
+     for (let i of todosLosSueldos) {
+          if (menor == 0)
+               menor = Number(i.value)
+          else if (Number(i.value) < menor) {
+               menor = Number(i.value)
           }
-          else if (menor == 0)
-               menor = i
-
-          if (i > mayor) {
-               mayor = i
+          if (Number(i.value) > mayor) {
+               mayor = Number(i.value)
           }
-          promedio = promedio + i
+          promedio = promedio + Number(i.value)
           divisor++
      }
 
      document.querySelector("div#resultado").innerText = "El sueldo promedio de tu hogar es de: " +
-          (sueldoPromedio /divisor) + ", el más bajo es de: " + sueldoMenor + ", y el mayor es de : " + sueldoMayor
+          (promedio / divisor) + ", el más bajo es de: " + menor + ", y el mayor es de : " +
+          mayor
 
 
+}
+
+let $calcularDeNuevo = document.querySelector("#empezar-de-nuevo")
+
+$calcularDeNuevo.onclick = function () {
+
+     document.querySelector("#cantidad-de-personas").style = "display:block"
+     document.querySelector("#boton-entrar").style = "display:block"
+
+     document.querySelector("#boton-calcular").style = "display:none"
+
+
+     let nodoPrincipal = document.querySelector("div#bienvenida")
+     let formsEnIndex= document.querySelectorAll("form")
+     
+     nodoPrincipal.removeChild(formsEnIndex)
 }
 
 
@@ -82,87 +95,85 @@ $botonCalcular = function () {
 
 
 
+// /*
+// TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
+// Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
+// Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y 
+// el promedio del grupo familiar.
 
-
-/*
-TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
-Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
-Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y 
-el promedio del grupo familiar.
-
-Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar
-     cómo en MDN).
-*/
+// Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar
+//      cómo en MDN).
+// */
 
 
 
 
 
 
-const $botonIngresar = document.querySelector("#ingresar")
-$botonIngresar.onclick = function () {
-     let numeroDelGrupoFamiliar = Number(document.querySelector("#numeroDelGrupoFamiliar").value);
-     let nodoPaginaTexto = document.querySelector('body')
-     let divFamilia = document.querySelector("div#familia")
+// const $botonIngresar = document.querySelector("#ingresar")
+// $botonIngresar.onclick = function () {
+//      let numeroDelGrupoFamiliar = Number(document.querySelector("#numeroDelGrupoFamiliar").value);
+//      let nodoPaginaTexto = document.querySelector('body')
+//      let divFamilia = document.querySelector("div#familia")
 
-     if (numeroDelGrupoFamiliar > 0 && numeroDelGrupoFamiliar != null) {
-          document.querySelector("div#familia").style = "display:block"
-     }
-     for (i = 1; i <= numeroDelGrupoFamiliar; i++) {
-          let formularioFamilia = document.querySelector('form#formulario-familiar')
-          let nuevoFormulario = document.createElement("form")
-          let nuevoInput = document.createElement("input")
-          nuevoInput.id = "inputFamilia"
-          nuevoInput.type = 'number'
+//      if (numeroDelGrupoFamiliar > 0 && numeroDelGrupoFamiliar != null) {
+//           document.querySelector("div#familia").style = "display:block"
+//      }
+//      for (i = 1; i <= numeroDelGrupoFamiliar; i++) {
+//           let formularioFamilia = document.querySelector('form#formulario-familiar')
+//           let nuevoFormulario = document.createElement("form")
+//           let nuevoInput = document.createElement("input")
+//           nuevoInput.id = "inputFamilia"
+//           nuevoInput.type = 'number'
 
-          let nuevoLabel = document.createElement("label")
-          nuevoLabel.innerText = ("Integrante " + i + ': ')
+//           let nuevoLabel = document.createElement("label")
+//           nuevoLabel.innerText = ("Integrante " + i + ': ')
 
-          formularioFamilia.appendChild(nuevoFormulario)
-          nuevoFormulario.appendChild(nuevoLabel)
-          nuevoFormulario.appendChild(nuevoInput)
+//           formularioFamilia.appendChild(nuevoFormulario)
+//           nuevoFormulario.appendChild(nuevoLabel)
+//           nuevoFormulario.appendChild(nuevoInput)
 
-     }
-     return false
-}
-
-
-let promediar = function (arrayDeNumeros, numeroParaPromediar) {
-     let cantidadDeNumeros = (arrayDeNumeros.length)
-     return (numeroParaPromediar / cantidadDeNumeros).toFixed(2)
-}
+//      }
+//      return false
+// }
 
 
-let $botonAceptar = document.querySelector("#aceptar")
-$botonAceptar.onclick = function () {
-     let edadesEnLaFamilia = document.querySelectorAll("#inputFamilia")
-     let mayorDeEdad = 0
-     let menorDeEdad = 0
-     let edadPromedio = 0
-     let edadesSumadas = 0
-     for (i = 0; i < edadesEnLaFamilia.length; i++) {
-          if (mayorDeEdad < edadesEnLaFamilia[i].value) {
-               mayorDeEdad = Number(edadesEnLaFamilia[i].value)
-          } if (menorDeEdad > edadesEnLaFamilia[i].value || menorDeEdad == 0) {
-               menorDeEdad = Number(edadesEnLaFamilia[i].value)
-          }
-          edadesSumadas += Number(edadesEnLaFamilia[i].value)
-     } edadPromedio = promediar(edadesEnLaFamilia, edadesSumadas)
-     let edadesRelevantes = document.querySelector("div#edades-relevantes")
-     edadesRelevantes.innerText = 'La edad promedio de tu familia es de: ' + edadPromedio + " años. " + 'La persona con menor edad tiene: ' + menorDeEdad + " años. " + 'La persona con mayor edad tiene: ' + mayorDeEdad + " años. "
-}
+// let promediar = function (arrayDeNumeros, numeroParaPromediar) {
+//      let cantidadDeNumeros = (arrayDeNumeros.length)
+//      return (numeroParaPromediar / cantidadDeNumeros).toFixed(2)
+// }
 
-let $limpiar = document.querySelector("#limpiar")
-$limpiar.onclick = function () {
-     document.querySelector("div#familia").style = "display:none"
-     let edadesRelevantes = document.querySelector("div#edades-relevantes")
-     edadesRelevantes.innerText = ''
-     let formularioFamiliar = document.querySelector("form#formulario-familiar")
-     while (formularioFamiliar.firstChild) {
-          formularioFamiliar.removeChild(formularioFamiliar.firstChild);
-     }
 
-}  //Los labels tambien se van por el hecho de ser un firstchild, ¿verdad?
+// let $botonAceptar = document.querySelector("#aceptar")
+// $botonAceptar.onclick = function () {
+//      let edadesEnLaFamilia = document.querySelectorAll("#inputFamilia")
+//      let mayorDeEdad = 0
+//      let menorDeEdad = 0
+//      let edadPromedio = 0
+//      let edadesSumadas = 0
+//      for (i = 0; i < edadesEnLaFamilia.length; i++) {
+//           if (mayorDeEdad < edadesEnLaFamilia[i].value) {
+//                mayorDeEdad = Number(edadesEnLaFamilia[i].value)
+//           } if (menorDeEdad > edadesEnLaFamilia[i].value || menorDeEdad == 0) {
+//                menorDeEdad = Number(edadesEnLaFamilia[i].value)
+//           }
+//           edadesSumadas += Number(edadesEnLaFamilia[i].value)
+//      } edadPromedio = promediar(edadesEnLaFamilia, edadesSumadas)
+//      let edadesRelevantes = document.querySelector("div#edades-relevantes")
+//      edadesRelevantes.innerText = 'La edad promedio de tu familia es de: ' + edadPromedio + " años. " + 'La persona con menor edad tiene: ' + menorDeEdad + " años. " + 'La persona con mayor edad tiene: ' + mayorDeEdad + " años. "
+// }
+
+// let $limpiar = document.querySelector("#limpiar")
+// $limpiar.onclick = function () {
+//      document.querySelector("div#familia").style = "display:none"
+//      let edadesRelevantes = document.querySelector("div#edades-relevantes")
+//      edadesRelevantes.innerText = ''
+//      let formularioFamiliar = document.querySelector("form#formulario-familiar")
+//      while (formularioFamiliar.firstChild) {
+//           formularioFamiliar.removeChild(formularioFamiliar.firstChild);
+//      }
+
+// }  //Los labels tambien se van por el hecho de ser un firstchild, ¿verdad?
 
 
 
