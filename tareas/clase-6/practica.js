0316feb52c499b5d831997eebdd10a6ccf5bbd3e
nahuel-fed <fgonz1998@gmail.com>
2020-03-02 17:57:39 -0300

    Vuelve a una versión vieja
    cometí errores que ahora tengo que arreglar

--- tareas/clase-6/practica.js
+++ tareas/clase-6/practica.js
@@ -8,3 +8,21 @@ $ingresar.onclick=function(){
 
 }
 
+$calcularDeNuevo.onclick = function () {
+
+    document.querySelector("#cantidad-de-personas").style = "display:block"
+    document.querySelector("#boton-entrar").style = "display:block"
+
+    document.querySelector("#boton-calcular").style = "display:none"
+
+
+    let nodoPrincipal = document.querySelector("form#resultado")
+    //let formsEnIndex= document.querySelector("form")
+
+    while (nodoPrincipal.firstChild) {
+         nodoPrincipal.removeChild(nodoPrincipal.firstChild);
+    }
+
+    document.querySelector("#empezar-de-nuevo").style = ("display:none")
+}
+
